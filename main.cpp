069005e8fe085889816993d0ebb36e9ebac158a8
Code-Adi2894 <rkl.adipatel2894@gmail.com>
2025-09-07 20:25:23 +0530

    Init files

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Person
+{
+    int age = 0;
+    int height = 20;
+    private: int weight = 30;
+};
+
+int main()
+{
+    const int x = 20;
+    printf("Hello, %d\n", x);
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
